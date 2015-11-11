8a1e6af56fcdfd16096401ae9ab2c123ba048a68
Fogetti <fogetti@freemail.hu>
2015-11-11 06:19:21 -0800

    Initial commit

---
+++ ExcelImporterTest01/StartForm.Designer.vb
@@ -0,0 +1,74 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class StartForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Import = New System.Windows.Forms.Button()
+        Me.Generate = New System.Windows.Forms.Button()
+        Me.Export = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Import
+        '
+        Me.Import.Location = New System.Drawing.Point(107, 62)
+        Me.Import.Name = "Import"
+        Me.Import.Size = New System.Drawing.Size(75, 23)
+        Me.Import.TabIndex = 0
+        Me.Import.Text = "Import"
+        Me.Import.UseVisualStyleBackColor = True
+        '
+        'Generate
+        '
+        Me.Generate.Location = New System.Drawing.Point(105, 120)
+        Me.Generate.Name = "Generate"
+        Me.Generate.Size = New System.Drawing.Size(75, 23)
+        Me.Generate.TabIndex = 1
+        Me.Generate.Text = "Generate"
+        Me.Generate.UseVisualStyleBackColor = True
+        '
+        'Export
+        '
+        Me.Export.Location = New System.Drawing.Point(107, 180)
+        Me.Export.Name = "Export"
+        Me.Export.Size = New System.Drawing.Size(75, 23)
+        Me.Export.TabIndex = 2
+        Me.Export.Text = "Export"
+        Me.Export.UseVisualStyleBackColor = True
+        '
+        'StartForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.Export)
+        Me.Controls.Add(Me.Generate)
+        Me.Controls.Add(Me.Import)
+        Me.Name = "StartForm"
+        Me.Text = "Welcome!"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Import As Button
+    Friend WithEvents Generate As Button
+    Friend WithEvents Export As Button
+End Class
---
+++ ExcelImporterTest01/StartForm.vb
@@ -0,0 +1,75 @@
+﻿Imports System.Data.OleDb
+Imports System.Data.SqlClient
+Public Class StartForm
+
+    Dim ds1 As New DataSet
+    Dim ds2 As New DataSet
+
+    Private Sub Import_Click(sender As Object, e As EventArgs) Handles Import.Click
+        Dim fd As OpenFileDialog = New OpenFileDialog()
+        Dim strFileName As String
+
+        fd.Title = "Open File Dialog"
+        fd.InitialDirectory = "C:\"
+        fd.Filter = "All files (*.*)|*.*|All files (*.*)|*.*"
+        fd.FilterIndex = 2
+        fd.RestoreDirectory = True
+
+        If fd.ShowDialog() = DialogResult.OK Then
+            strFileName = fd.FileName
+            Dim _conn As String
+            _conn = "Provider=Microsoft.Jet.OLEDB.4.0;" & "Data Source=" & strFileName & ";" & "Extended Properties=Excel 8.0;"
+            Dim _connection As OleDbConnection = New OleDbConnection(_conn)
+            Dim da As OleDbDataAdapter = New OleDbDataAdapter()
+            Dim _command As OleDbCommand = New OleDbCommand()
+            _command.Connection = _connection
+            _command.CommandText = "SELECT * FROM [Introduction$]"
+            da.SelectCommand = _command
+            Try
+                da.Fill(ds1, "sheet1")
+                MessageBox.Show("The import is complete!")
+                'Me.DataGridView1.DataSource = ds1
+                'Me.DataGridView1.DataMember = "sheet1"
+            Catch e1 As Exception
+                MessageBox.Show("Import Failed, correct Column name in the sheet!")
+            End Try
+        End If
+    End Sub
+
+    Dim da As SqlDataAdapter
+    Dim conn As SqlConnection
+    Dim cb As SqlCommandBuilder
+
+    Private Sub Generate_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Generate.Click
+        conn = New SqlConnection("Data Source=Servername;Initial Catalog=mydb;Integrated Security=True")
+        Dim sel As String = "SELECT * FROM myTable"
+        da = New SqlDataAdapter(sel, conn)
+        cb = New SqlCommandBuilder(da)
+        da.MissingSchemaAction = MissingSchemaAction.AddWithKey
+        da.Fill(ds2, "myTable")
+        'Me.DataGridView1.DataSource = ds2
+        'Me.DataGridView1.DataMember = "myTable"
+    End Sub
+
+    Private Sub Export_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Export.Click
+        For Each dr As DataRow In ds1.Tables(0).Rows
+            Dim expression As String
+            expression = "myId =" + CType(dr.Item(0), Integer).ToString
+            Dim drs() As DataRow = ds2.Tables(0).Select(expression)
+            If (drs.Length = 1) Then
+                For i As Integer = 1 To ds2.Tables(0).Columns.Count - 1
+                    drs(0).Item(i) = dr.Item(i)
+                Next
+            Else
+                Dim drnew As DataRow = ds2.Tables(0).NewRow
+                For i As Integer = 0 To ds2.Tables(0).Columns.Count - 1
+                    drnew.Item(i) = dr.Item(i)
+                Next
+                ds2.Tables(0).Rows.Add(drnew)
+            End If
+        Next
+        'Me.DataGridView1.DataSource = ds2
+        'Me.DataGridView1.DataMember = "myTable"
+        da.Update(ds2.Tables(0))
+    End Sub
+End Class
